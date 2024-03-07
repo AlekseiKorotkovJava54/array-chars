21b714a6aea0e2c2b5bece28ab130d3388fb7c34
AlekseiKorotkov159 <158587440+AlekseiKorotkov159@users.noreply.github.com>
2024-03-07 21:06:17 +0200

    cw5 ArrayChar

--- src/telran/arrays/test/ArrayCharTest.java
+++ src/telran/arrays/test/ArrayCharTest.java
@@ -19,7 +19,21 @@ class ArrayCharTest {
 		assertEquals(8, arrayChar2.compareTo(arrayChar1));
 		assertEquals(-8, arrayChar1.compareTo(arrayChar2));
 		assertEquals(1, new ArrayChar(new char[] {'a','a'}).compareTo(new ArrayChar(new char[] {'a'})));
-		assertEquals(1, new ArrayChar(new char[] {'a'}).compareTo(new ArrayChar(new char[] {'a','a'})));
+		assertEquals(-1, new ArrayChar(new char[] {'a'}).compareTo(new ArrayChar(new char[] {'a','a'})));
 		assertEquals(0, arrayChar1.compareTo(arrayChar3));
 		}
+	@Test
+	void indexOfTest() {
+		char[] ar1 = {'1','2','3','2'};
+		ArrayChar arrayChar1 = new ArrayChar(ar1);
+		assertEquals(1, arrayChar1.indexOf('2'));
+		assertEquals(-1, arrayChar1.indexOf('9'));
+	}
+	@Test
+	void lastIndexOfTest() {
+		char[] ar1 = {'1','2','3','2'};
+		ArrayChar arrayChar1 = new ArrayChar(ar1);
+		assertEquals(3, arrayChar1.lastIndexOf('2'));
+		assertEquals(-1, arrayChar1.lastIndexOf('9'));
+	}
 }
